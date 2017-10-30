b4a38897f8dc412d5b2a2f9d4e2b5bf233075c0d
RedFox <282966511@qq.com>
2017-10-30 15:37:44 +0800

    初始化

---
+++ DYZB/DYZB/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DYZB
+//
+//  Created by 钱恒强 on 2017/10/30.
+//  Copyright © 2017年 钱恒强. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
